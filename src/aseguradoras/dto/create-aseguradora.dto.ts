9168c776aa33b281db4973e495046ec78d0f2d27
Emmanuel <flotuz10@gmail.com>
2022-04-20 22:29:40 -0500

    Validadores del modelo Aseguradora

--- src/aseguradoras/dto/create-aseguradora.dto.ts
+++ src/aseguradoras/dto/create-aseguradora.dto.ts
@@ -1,21 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
 
 export class CreateAseguradoraDto {
-  @ApiProperty()
-  id: number;
-
+  @IsNotEmpty()
   @ApiProperty()
   nombre: string;
 
+  @IsNotEmpty()
+  @IsPhoneNumber()
   @ApiProperty()
   telefono: string;
 
+  @IsNotEmpty()
   @ApiProperty()
   expediente: string;
-
-  @ApiProperty()
-  createdAt: Date;
-
-  @ApiProperty()
-  updatedAt: Date;
 }
