7ebd6306101a27756b628cc00f6fa5b59ce6e70b
Emmanuel <flotuz10@gmail.com>
2022-04-20 22:05:08 -0500

    Validadores del modelo Seguimiento

--- src/seguimientos/dto/create-seguimiento.dto.ts
+++ src/seguimientos/dto/create-seguimiento.dto.ts
@@ -1,21 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateSeguimientoDto {
+  @IsNotEmpty()
   @ApiProperty()
   nombre_asesor_seguro: string;
 
+  @IsNotEmpty()
   @ApiProperty()
   detalles: string;
 
+  @IsNotEmpty()
   @ApiProperty()
   fecha_seguimiento: Date;
 
+  @IsNotEmpty()
   @ApiProperty()
   ticketId: number;
 
+  @IsNotEmpty()
   @ApiProperty()
   usuarioId: number;
-
-  @ApiProperty()
-  createdAt: Date;
 }
