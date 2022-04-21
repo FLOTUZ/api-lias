ac8c967db21038ffd2e484eb04887f28f8daaefc
Emmanuel <flotuz10@gmail.com>
2022-04-20 21:58:25 -0500

    Validadores del modelo Servicio

--- src/servicios/dto/create-servicio.dto.ts
+++ src/servicios/dto/create-servicio.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateServicioDto {
+  @IsNotEmpty()
   @ApiProperty({ example: 'Plomeria' })
   nombre: string;
 
+  @IsNotEmpty()
   @ApiProperty({ example: 'DOMESTICO', enum: ['DOMESTICO', 'VIAL'] })
   tipo: string;
 }
