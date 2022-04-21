bf50a26f0933d448c0ea62c260d8f81ea2937e4e
Emmanuel <flotuz10@gmail.com>
2022-04-20 22:20:05 -0500

    Validadores del modelo Asistencia

--- src/asistencias/dto/create-asistencia.dto.ts
+++ src/asistencias/dto/create-asistencia.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateAsistenciaDto {
+  @IsNotEmpty()
   @ApiProperty()
   nombre: string;
 
+  @IsNotEmpty()
+  @IsNumber()
   @ApiProperty()
   aseguradoraId: number;
 }
