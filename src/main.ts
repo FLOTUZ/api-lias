9bfbfda52ddeaccf04b1a79c5d55b67ffbe08e4f
Emmanuel <flotuz10@gmail.com>
2022-06-11 18:54:10 -0500

    Fix Image retrive

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
 
   app.setViewEngine('hbs');
   app.useStaticAssets(join(__dirname, '../public'));
+  app.useStaticAssets(join(__dirname, '../uploads'));
   app.setBaseViewsDir(join(__dirname, '../public/views'));
 
   const { httpAdapter } = app.get(HttpAdapterHost);
--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
-import { TicketEntity } from './entities/ticket.entity';
 
 @Module({
   controllers: [TicketsController],
